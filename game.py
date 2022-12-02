8e8d74444b4ff5944fc962bba508916e2276c755
ThePotter <nguyentadac@gmail.com>
2022-12-02 20:13:21 +0700

    v0.1

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
+import random
 
+print("""
+Game rule:
+You have a fish with a certain value (from 2 to 9)
+You have a random set of choices
+""")
+
+exit = False
+round = 1
+fishes = []
+while not exit:
+    startFish = random.randint(2, 9)
+    choice = 0
+    numEnemyFish = random.randint(3, 9)
+    for i in range(numEnemyFish):
+        fishes.append(random.randint(1, 9))
+
+    if startFish <= min(fishes):
+        fishes.clear()
+        continue
